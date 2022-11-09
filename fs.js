58868d18c8a6f852bc8815fecd087f0ab31ee45e
Julio <trabajos386@gmail.com>
2022-11-08 22:54:36 -0600

    Conoci fs asincrono aun conocer promesas para eficiente el codigo, esto se actualizara mas adelante con promesas

--- fs.js
+++ fs.js
@@ -1,5 +1,5 @@
-const fs = require('fs')
-
+const fs = require("fs");
+/*
 const first = fs.readFileSync('./data/first.txt', 'utf-8')
 const second = fs.readFileSync('./data/second.txt')
 
@@ -10,3 +10,44 @@ console.log(second.toString)
 fs.writeFileSync('./data/third.txt',' Este es un tercer archivo creado desde fs.js',{
     flag: 'a'
 })
+*/
+
+fs.readFile("./data/first.txt", (error, data) => {
+  if (error) {
+    console.log(error);
+  }
+  console.log(data.toString());
+
+  fs.readFile("./data/second.txt", (error, data) => {
+    if (error) {
+      console.log(error);
+    }
+    console.log(data.toString());
+    fs.writeFile(
+      "./data/newFile.txt",
+      "archivo creado desde fs",
+      (err, data) => {
+        console.log(err);
+        console.log(data);
+
+        fs.writeFile(
+          "./data/newFile2.txt",
+          "archivo creado desde fs",
+          (err, data) => {
+            console.log(err);
+            console.log(data);
+
+            fs.writeFile(
+              "./data/newFile3.txt",
+              "archivo creado desde fs",
+              (err, data) => {
+                console.log(err);
+                console.log(data);
+              }
+            );
+          }
+        );
+      }
+    );
+  });
+});
